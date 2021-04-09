907764310e01b580512aa47591a08da9ccf3615e
GuliyD <guliy.ememrald@gmail.com>
2021-04-09 22:55:33 +0200

    new get info

--- take_data/urls.py
+++ take_data/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_data, calculator
+from .views import get_data, calculator, get_info
 
 urlpatterns = [
     path('<str:text1>/<int:num1>/<str:text2>/<int:num2>/', get_data),
-    path('calculator/<int:num1>/<int:num2>', calculator)
+    path('calculator/<int:num1>/<int:num2>/<str:dz>', calculator),
+    path('get_info', get_info)
 ]
--- take_data/views.py
+++ take_data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.utils import translation
 from .models import UserData
 
 
@@ -9,5 +9,21 @@ def get_data(request, text1, num1, text2, num2):
     return render(request, 'take_data/index.html', {'model': model})
 
 
-def calculator(request, num1, num2):
-    return render(request, 'take_data/calculator.html', {'result': num1+num2})
+def calculator(request, num1, num2, dz):
+    if dz == 'plus':
+        result = num1+num2
+    if dz == 'minus':
+        result = num1-num2
+    if dz == 'iloczyn':
+        result = num1*num2
+    return render(request, 'take_data/calculator.html', {'result': result})
+
+def get_info(request):
+    user_agent = request.META.get('HTTP_USER_AGENT')
+    language = translation.get_language()
+    cookie = request.COOKIES
+    host = request.META.get('HTTP_HOST')
+    response = render(request, 'take_data/get_info.html', {'user_agent': user_agent, 'language': language, 'cookie': cookie, 'host': host}
+                      )
+    response.set_cookie('qwe', 'this is cookie')
+    return response
